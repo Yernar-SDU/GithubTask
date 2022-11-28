1d1204a6b2720ff50a27172fdaee18fab1aa717f
Beisen <113160775+Beisen7@users.noreply.github.com>
2022-11-28 20:28:03 +0600

    Update Questions.java

--- src/Questions.java
+++ src/Questions.java
@@ -1,11 +1,11 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: _______");
-        System.out.println("Surname: _______");
-        System.out.println("Id: _______");
-        System.out.println("Hobby: _______");
-        System.out.println("How much do you like ICT(from 1-10)): _______");
-        System.out.println("Rate this task from 1 to 10: _______");
-        System.out.println("Am I the best teacher?: _______");
+        System.out.println("Name: Beisen");
+        System.out.println("Surname: Adilov");
+        System.out.println("Id: 220103323");
+        System.out.println("Hobby: Basketball");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 10");
+        System.out.println("Am I the best teacher?: Yes");
     }
 }
