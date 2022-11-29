8efb6c37b10527063c3d515a3677d6466bfa8be4
Darkhan <darxghostkz@gmail.com>
2022-11-29 23:54:07 +0600

    Darhan made commit

--- src/Questions.java
+++ src/Questions.java
@@ -1,11 +1,11 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: _______");
-        System.out.println("Surname: _______");
-        System.out.println("Id: _______");
-        System.out.println("Hobby: _______");
-        System.out.println("How much do you like ICT(from 1-10)): _______");
-        System.out.println("Rate this task from 1 to 10: _______");
-        System.out.println("Am I the best teacher?: _______");
+        System.out.println("Name: Darhan");
+        System.out.println("Surname: Nazarov");
+        System.out.println("Id: 220103319");
+        System.out.println("Hobby: watching anime");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 10");
+        System.out.println("Am I the best teacher?: Yes, you are!");
     }
 }
