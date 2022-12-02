b9513946b4cd1a11e95b1083a0a856019a22907b
Rakhat Zhardemov <220107040@stu.sdu.edu.kz>
2022-12-03 00:29:14 +0300

    Updateee

--- src/Questions.java
+++ src/Questions.java
@@ -1,11 +1,11 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: _______");
-        System.out.println("Surname: _______");
-        System.out.println("Id: _______");
-        System.out.println("Hobby: _______");
-        System.out.println("How much do you like ICT(from 1-10)): _______");
-        System.out.println("Rate this task from 1 to 10: _______");
-        System.out.println("Am I the best teacher?: _______");
+        System.out.println("Name: Rakhat");
+        System.out.println("Surname: Zhardemov");
+        System.out.println("Id: 220107040");
+        System.out.println("Hobby: Guitar, Dombyra");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 10");
+        System.out.println("Am I the best teacher?: Yes, Yernar agai top, zhez ktl top");
     }
 }
